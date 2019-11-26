9cc157e693d5f88528670bb0579e17cc78e0d2db
Kracav4ik <dikama2013@yandex.ru>
2019-11-26 06:58:44 +0300

    refactor ExpressionParser

--- src/parser/ExpressionParser.h
+++ src/parser/ExpressionParser.h
@@ -1,20 +1,52 @@
 #pragma once
 
 #include "parser/ast/ASTNode.h"
+#include <map>
+#include <vector>
 
 class Lexer;
+class ExpressionParser;
+
+enum class Priority {
+    INCLUSIVE_OR_EXPRESSION,
+    EXCLUSIVE_OR_EXPRESSION,
+    AND_EXPRESSION,
+    SHIFT_EXPRESSION,
+    ADDITIVE_EXPRESSION,
+    MULTIPLICATIVE_EXPRESSION,
+    UNARY_EXPRESSION,
+    PRIMARY_EXPRESSION,
+};
+
+class ExpressionParserDLC {
+public:
+    ExpressionParserDLC(Priority priority);
+
+    virtual ~ExpressionParserDLC() = default;
+    virtual ASTNodePtr try_parse(Lexer& lexer, ExpressionParser& expressionParser) = 0;
+    Priority getPriority() const;
+
+private:
+    Priority _priority;
+};
 
 class ExpressionParser {
 public:
+    using DLC = ExpressionParserDLC;
+
+    ExpressionParser();
+
     ASTNodePtr try_expression(Lexer& lexer);
 
+    ASTNodePtr try_next_priority(Lexer& lexer, Priority priority);
+    ASTNodePtr try_same_priority(Lexer& lexer, Priority priority);
+
+    void add_dlc(std::shared_ptr<DLC> dlc);
+
 private:
-    ASTNodePtr try_inclusive_or_expression(Lexer& lexer);
-    ASTNodePtr try_exclusive_or_expression(Lexer& lexer);
-    ASTNodePtr try_and_expression(Lexer& lexer);
-    ASTNodePtr try_shift_expression(Lexer& lexer);
-    ASTNodePtr try_additive_expression(Lexer& lexer);
-    ASTNodePtr try_multiplicative_expression(Lexer& lexer);
-    ASTNodePtr try_unary_expression(Lexer& lexer);
-    ASTNodePtr try_primary_expression(Lexer& lexer);
+    using DlcMap = std::map<Priority, std::vector<std::shared_ptr<DLC>>>;
+
+    ASTNodePtr try_parsers(Lexer& lexer, DlcMap::const_iterator parsers_it);
+
+    DlcMap _dlc_map;
 };
